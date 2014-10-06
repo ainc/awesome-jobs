ae41b236b80c5c60f1cb4ee6fd975c43ba27ba7d
Alek <alekrzieba@gmail.com>
2014-10-06 08:03:55 -0400

    Fix Lockable Issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :confirmable, :database_authenticatable, :registerable,
+  devise :confirmable, :database_authenticatable, :lockable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :jobs, dependent: :destroy
