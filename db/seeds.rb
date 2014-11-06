835cbc68db82650651e6e9942047030530196ec0
Alek <alekrzieba@gmail.com>
2014-11-06 15:04:59 -0500

    Remove Extraneous Button from Other Users' Page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Based on the Trello comments
+hagan = User.new(username: 'hagan', password: 'password', email: 'weston@durhamlabs.com', name: 'Weston Hagan', company: 'Durham Labs')
+hagan.skip_confirmation!
+hagan.save!
+
+hagan_job = Job.new(user: hagan, title: 'Engineer', description: 'Doing some engineering.' ,url: 'http://insiderlouisville.com/startups/cool-stuff/fred-durham-wants-play-food-robots-part-two-robots/')
+hagan_job.save!
+
+
+
