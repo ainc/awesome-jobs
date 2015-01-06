bb13235c921dfb7e1d4aa4c38e6277bbb07c91c9
Alek <alekrzieba@gmail.com>
2015-01-06 17:38:37 -0500

    Configure for Amazon AWS

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -7,12 +7,13 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/avatars/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   def default_url
