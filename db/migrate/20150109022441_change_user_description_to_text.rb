3db9cd3c89ec4a4d5190231f0198dd48751ca40c
Alek <alekrzieba@gmail.com>
2015-01-08 21:25:36 -0500

    Change User Descriptions to Texts instead of Strings

---
+++ db/migrate/20150109022441_change_user_description_to_text.rb
@@ -0,0 +1,10 @@
+class ChangeUserDescriptionToText < ActiveRecord::Migration
+  def up
+    change_column :users, :description, :text
+  end
+  def down
+    # This might cause trouble if you have strings longer
+    # than 255 characters.
+    change_column :users, :description, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150106222121) do
+ActiveRecord::Schema.define(version: 20150109022441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(version: 20150106222121) do
     t.datetime "locked_at"
     t.string   "name"
     t.boolean  "is_employer"
-    t.string   "description"
+    t.text     "description"
     t.string   "company"
     t.string   "phone"
     t.string   "address"
